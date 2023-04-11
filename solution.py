3605393a51f78770689f9f265008dad02ffc9731
Leontiy Tsukanov <122561265+LeontiyTsukanov@users.noreply.github.com>
2023-04-11 22:13:30 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 407415686 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    flag = False
+    if MMD(compute_kernel = "rbf", gamma = 0.7).test(h[a], m3[b])[1] >= 0.03:
+        flag = True
+    return flag # Ваш ответ, True или False
